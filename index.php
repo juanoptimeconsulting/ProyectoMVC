6978ad968ae420d0179f62eca527a50d3878b51e
juanoptime <jdiaz@optimeconsulting.com>
2018-11-20 17:13:30 -0500

    edited the insert query

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 //obligatrio para el funcionamiento del programa, funciona de igual que require, pero con le once para requerir un vaez
 require_once "controllers/controlador.php";//conexion al index php
 require_once "models/modelo.php";
+require_once "models/crud.php";
 
 
 
