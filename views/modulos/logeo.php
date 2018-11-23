5e45c8452d6acb99a675affd58b595ad29a82f27
juanoptime <jdiaz@optimeconsulting.com>
2018-11-23 12:14:16 -0500

    prueba2

--- views/modulos/logeo.php
+++ views/modulos/logeo.php
@@ -12,10 +12,9 @@
 $objetollegada = new mvcontroller();
 
 
-if(isset($_GET["action"])) {
+if(isset($_GET["action"]) ) {
     //se coloco con action == erroringreso porque no volvia a validar al comeeter un error en el formulario y gracias este condicional ya entra y valida
-
-    if ($_GET["action"] == "logeo" || $_GET["action"]=="erroringreso") {
+    if ($_GET["action"] == "logeo" || $_GET["action"]=="erroringreso" ) {
 
         $objetollegada->logeo();
 
@@ -30,40 +29,47 @@ if(isset($_GET["action"])) {
 
 ?>
 
-<div class="row">
-    <form class="col s12" method="post">
-        <div class="row">
-            <div class="input-field col s6">
-                <input name="UserIngreso" id="input_text" type="text" data-length="10">
-                <label for="input_text">Input text</label>
-            </div>
+
+<form class="col s12" method="post">
+    <div class="row">
+        <div class="input-field col s6">
+            <input name="UserIngreso" id="input_text" type="text" data-length="10">
+            <label for="input_text">Input text</label>
         </div>
-<div class="row">
-            <div class="input-field col s12">
-                <input name="PasswordIngreso" id="password" type="password" class="validate">
-                <label for="password">Password</label>
-            </div>
+    </div>
+    <div class="row">
+        <div class="input-field col s12">
+            <input name="PasswordIngreso" id="password" type="password" class="validate">
+            <label for="password">Password</label>
         </div>
-
-        <button type="submit"  class="btn btn-default">Enviar</button>
-
-        <?php   if($_GET["action"]=="erroringreso"){?>
+    </div>
 
 
-            <div class="row">
-                <div class="col s12 m5">
-                    <div  class="card-panel teal  center-align" >
+
+
+    <button type="submit"  class="btn btn-default">Enviar</button>
+
+
+
+</form>
+
+
+<?php if( $_GET["action"]=="erroringreso" )
+{
+    ?>
+    <div class="row">
+        <div class="col s12 m5">
+            <div  class="card-panel teal  center-align" >
         <span class="white-text">ERROR DE INICIO
         </span>
-                    </div>
-                </div>
             </div>
+        </div>
+    </div>
+    <?php
+}?>
 
-        <?php } ?>
 
 
-
-    </form>
 
 
 
